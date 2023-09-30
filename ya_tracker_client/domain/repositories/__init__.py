d66a81db839cf59f13b3a6a4d208b462e47b8ad1
Дмитрий Анфимов <danfimov@yandex-team.ru>
2023-09-30 23:28:29 +0300

    feat: create all repositories

--- ya_tracker_client/domain/repositories/__init__.py
+++ ya_tracker_client/domain/repositories/__init__.py
@@ -1,9 +1,14 @@
 from ya_tracker_client.domain.repositories.attachment import AttachmentRepository
+from ya_tracker_client.domain.repositories.bulk_operation import BulkOperationRepository
 from ya_tracker_client.domain.repositories.checklist import ChecklistRepository
 from ya_tracker_client.domain.repositories.comment import CommentRepository
 from ya_tracker_client.domain.repositories.component import ComponentRepository
+from ya_tracker_client.domain.repositories.external_link import ExternalLinkRepository
+from ya_tracker_client.domain.repositories.importing import ImportingRepository
 from ya_tracker_client.domain.repositories.issue import IssueRepository
+from ya_tracker_client.domain.repositories.issue_field import IssueFieldRepository
 from ya_tracker_client.domain.repositories.issue_relationship import IssueRelationshipRepository
+from ya_tracker_client.domain.repositories.macros import MacrosRepository
 from ya_tracker_client.domain.repositories.project import ProjectRepository
 from ya_tracker_client.domain.repositories.queue import QueueRepository
 from ya_tracker_client.domain.repositories.user import UserRepository
@@ -12,11 +17,16 @@ from ya_tracker_client.domain.repositories.worklog import WorklogRepository
 
 __all__ = [
     "AttachmentRepository",
+    "BulkOperationRepository",
     "ChecklistRepository",
     "CommentRepository",
     "ComponentRepository",
+    "ExternalLinkRepository",
+    "ImportingRepository",
+    "IssueFieldRepository",
     "IssueRelationshipRepository",
     "IssueRepository",
+    "MacrosRepository",
     "ProjectRepository",
     "QueueRepository",
     "UserRepository",
